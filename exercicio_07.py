35dec85bbebc75b8f34bbd67296ac44751c1e70d
Julia Adão <jubza@edu.univali.br>
2017-10-20 15:51:36 -0200

    'exercicio_07' finalizado.

---
+++ exercicio_07.py
@@ -0,0 +1,20 @@
+print("O nome do meu bebê é {}.".format('Gaia'))
+print("Meu segundo bebê é o {}.".format("Gu"))
+print("= " * 15)
+
+var1 = "L"
+var2 = "O"
+var3 = "V"
+var4 = "E"
+var5 = " "
+var6 = "Y"
+var7 = "O"
+var8 = "U"
+var9 = "B"
+var10 = "O"
+var11 = "T"
+var12 = "H"
+var13 = "!!!"
+
+print(var1 + var2 + var3 + var4 + var5 + var6 + var7 + var8, end = ' ')
+print(var9 + var10 + var11 + var12 + var13)
