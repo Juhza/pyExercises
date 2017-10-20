97fd8f1305ddad8e35c7ad3213fa7983d3f9bf0b
Julia Adão <jubza@edu.univali.br>
2017-10-20 13:58:57 -0200

    'exercicio_04.py' finalizado.

---
+++ exercicio_04.py
@@ -0,0 +1,10 @@
+pagamento = 647.7
+adiantamento = 497.8
+bonus = 150
+vale_alimentacao = 400
+
+print("Pagamento do dia 05 - ", pagamento, " reais.")
+print("Adiantamento do dia 20 - ", adiantamento, " reais.")
+print("Total: ", pagamento + adiantamento)
+print("Total com bônus de presença: ", pagamento + adiantamento + bonus)
+print("Vale alimentação - ", vale_alimentacao, ", apenas para restaurantes.")
